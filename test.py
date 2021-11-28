fe671e93be2d38b560ec38d28d1893b6a582570f
HrtLock <88733735+HrtLock@users.noreply.github.com>
2021-11-29 00:16:08 +0300

    Update test.py

--- test.py
+++ test.py
@@ -61,3 +61,67 @@ for i in range(1, b + 1):
 
 
 
+#для 2
+n = 125
+a = 7
+b = 117
+num = 60
+print(f'Номер {num}.19')
+def f(x, y, p):
+    if x + y >= n and p == 3: # Выигрыш на 3-ем ходу игры
+        return True
+    elif x + y < n and p == 3: # Проигрыш на 3-ем ходу игры
+        return False
+    return f(x + 1, y, p + 1) or f(x * 4, y, p + 1) or f(x, y + 1, p + 1) or f(x, y * 4, p + 1)
+    # При каком-либо исходе игры игрок выигрывает
+
+
+for i in range(1, b + 1):
+    if f(a, i, 1):
+        print(i)
+        break
+
+print(f'Номер {num}.20')
+def f(x, y, p):
+    if x + y >= n and p == 4: # Выигрыш на 4-ом ходу игры
+        return True
+    else:
+        if x + y < n and p == 4: # Проигрыш на 4-ом ходу игры
+            return False
+        elif x + y >= n: # Выигрыш на ином ходу игры
+            return False
+
+    if p % 2 == 1:
+        return f(x + 1, y, p + 1) or f(x * 4, y, p + 1) or f(x, y + 1, p + 1) or f(x, y * 4, p + 1)
+        # При каком-либо исходе игрока 1, игрок выигрывает
+    else:
+        return f(x + 1, y, p + 1) and f(x * 4, y, p + 1) and f(x, y + 1, p + 1) and f(x, y * 4, p + 1)
+        # При любом исходе игрока 2 , игрок выигрывает
+
+for i in range(1, b + 1):
+    if f(a, i, 1):
+        print(i)
+
+print(f'Номер {num}.21')
+def f(x, y, p):
+    if x + y >= n and (p == 3 or p == 5): # может выиграть на 3 или 5 ходу
+        return True
+    else:
+        if x + y < n and p == 5: # не может проиграть на 5-ом ходу
+            return False
+        elif x + y >= n:
+            return False
+
+    if p % 2 == 0:
+        return f(x + 1, y, p + 1) or f(x * 4, y, p + 1) or f(x, y + 1, p + 1) or f(x, y * 4, p + 1)
+        # При каком-либо исходе игрока 2, игрок выигрывает
+    else:
+        return f(x + 1, y, p + 1) and f(x * 4, y, p + 1) and f(x, y + 1, p + 1) and f(x, y * 4, p + 1)
+        # При любом исходе игрока 1 , игрок выигрывает
+
+for i in range(1, b + 1):
+    if f(a, i, 1):
+        print(i)
+
+
+
